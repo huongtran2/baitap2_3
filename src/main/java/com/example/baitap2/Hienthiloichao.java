d951de389fb653ea26e4e59e115ae31695821cd5
huongtran2 <phuongtranth8888@gmail.com>
2022-10-27 09:39:37 +0700

    huong

---
+++ src/main/java/com/example/baitap2/Hienthiloichao.java
@@ -0,0 +1,36 @@
+package com.example.baitap2;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet1", value = "/chao")
+public class Hienthiloichao extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println(username);
+        System.out.println(password);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("huong".equals(username) && "huong".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+}
---
+++ src/main/webapp/bai2.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Tran Thi Thu Hang
+  Date: 27/10/2022
+  Time: 9:16 SA
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>bai 2</h1>
+<a href="chao"></a>
+</body>
+</html>
